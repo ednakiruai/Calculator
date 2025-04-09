f67533e6a2cc4c078b4734b2fa6e8d6c3e3b61be
Edna Kiruai <162158368+ednakiruai@users.noreply.github.com>
2025-04-09 23:01:42 +0300

    Delete Calculator.py
    Signed-off-by: Edna Kiruai <162158368+ednakiruai@users.noreply.github.com>

--- Calculator.py
+++
@@ -1,5 +0,0 @@
-def clear_field():
-    """Clears the calculation field and resets the stored calculation."""
-    global calculation
-    calculation = ""  # Reset the calculation string
-    text_result.delete(1.0, "end")  # Clear the display
